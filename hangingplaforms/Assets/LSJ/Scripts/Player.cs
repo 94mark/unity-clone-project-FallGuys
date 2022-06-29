715d6c30b8d91d649f4f5e1beed5ed07fb240871
Seongju <seongju0007@gmail.com>
2022-06-29 15:11:34 +0900

    Update

---
+++ hangingplaforms/Assets/LSJ/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 dir;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
